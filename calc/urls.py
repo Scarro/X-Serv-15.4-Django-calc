1f88d7c881d17f9b70d42891afef4d891599d780
Scarro <sergioca22@gmail.com>
2017-03-28 22:59:05 +0200

    actualizado

--- calc/urls.py
+++ calc/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
+
 from . import views
 
 app_name = 'calc'
 urlpatterns = [
-	url(r'^(?P<numero1>\-?\d+)\+(?P<numero2>\-?\d+)$', views.suma, name="suma"),
-	url(r'^(?P<numero1>\-?\d+)\-(?P<numero2>\-?\d+)$', views.resta, name="resta"),
-	url(r'^(?P<numero1>\-?\d+)\*(?P<numero2>\-?\d+)$', views.multiplicacion, name="multiplicacion"),
-	url(r'^(?P<numero1>\-?\d+)\/(?P<numero2>\-?\d+)$', views.division, name="division"),
+    url(r'^$', views.calculadora, name="calculadora"),
+    url(r'^(?P<operando1>-?[0-9]+)(?P<operacion>[\+\-\*\/])(?P<operando2>-?[0-9]+)$', views.operacion, name='sumar'),
 ]
--- calc/views.py
+++ calc/views.py
@@ -1,42 +1,31 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
 
-def suma(request, numero1, numero2):
-	suma = int(numero1) + int(numero2)
-	context = {
-		'operacion' : 'suma',
-		'resultado' : suma,
-	}
-	return render(request, 'calc/index.html',context)
+def calculadora(request):
+    return render(request, 'calc/index.html')
 
-def resta(request, numero1, numero2):
-	resta = int(numero1) - int(numero2)
-	context = {
-		'operacion' : 'resta',
-		'resultado' : resta
-	}
-	return render(request, 'calc/index.html', context)
-
-def multiplicacion(request, numero1, numero2):
-	mult = int(numero1) * int(numero2)
-	context = {
-		'operacion' : 'multiplicacion',
-		'resultado' : mult
-	}
-	return render(request, 'calc/index.html', context)
-
-def division (request, numero1, numero2):
-	try:
-		division = int(numero1) / int(numero2)
-		context = {
-			'operacion': 'division',
-			'resultado': division
-		}
-		template = 'calc/index.html'
-	except ZeroDivisionError:
-		context = {
-			'error': 'Error: divides entre 0'
-		}
-		template = 'calc/error.html'
-	return render(request, template, context)
+def operacion(request, operando1, operacion, operando2):
+    if operacion == '+':
+        op = "Suma"
+        resultado = int(operando1) + int(operando2)
+    elif operacion == '-':
+        op = "Resta"
+        resultado = int(operando1) - int(operando2)
+    elif operacion == '*':
+        op = "Multiplicación"
+        resultado = int(operando1) * int(operando2)
+    else:
+        try:
+            resultado = int(operando1) / int(operando2)
+        except ZeroDivisionError:
+            raise Http404('Error: imposible dividir entre 0')
+        op = "División"
+    return render(request, 'calc/operacion.html', {
+        'operacion': op,
+        'simbolo': operacion,
+        'operando1': operando1,
+        'operando2': operando2,
+        'resultado': resultado,
+        })
